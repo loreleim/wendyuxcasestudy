d1d08351fbe00e38228b5eb21d3284038e129ac7
loreleim <lorelei.miyamura@gmail.com>
2020-02-28 17:17:18 -0600

    initial home page

---
+++ wendy-ux-case-study/src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./index.module.scss";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className={style.mainContainer}>
+        <div>HomePage</div>
+      </div>
+    );
+  }
+}
+
+export default Home;
