13badd865181b0653c557c3b3cc488c60c69990c
loreleim <lorelei.miyamura@gmail.com>
2020-02-28 17:18:24 -0600

    setup of the main routes rended in the app page

--- wendy-ux-case-study/src/App.js
+++ wendy-ux-case-study/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/nav/index.js";
+import Home from "./pages/home/index.js";
+import Planning from "./pages/planning/index.js";
+import Production from "./pages/production/index.js";
+import UserTesting from "./pages/user-testing/index.js";
+import Launch from "./pages/launch/index.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Did Wendy Go Hiking Tho?</h1>
+    <div>
+      <Router basename="/wendygoeshiking">
+        <Nav></Nav>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/planning" component={Planning}></Route>
+          <Route path="/production" component={Production}></Route>
+          <Route path="/usertesting" component={UserTesting}></Route>
+          <Route path="/launch" component={Launch}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
