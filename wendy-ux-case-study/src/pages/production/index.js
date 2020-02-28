436e891eaf9ee3dcf16e2c36066ac5e1bd03213f
loreleim <lorelei.miyamura@gmail.com>
2020-02-28 17:16:24 -0600

    initial production page

---
+++ wendy-ux-case-study/src/pages/production/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./index.module.scss";
+
+class Production extends React.Component {
+  render() {
+    return (
+      <div className={style.mainContainer}>
+        <div>Production</div>
+      </div>
+    );
+  }
+}
+
+export default Production;
