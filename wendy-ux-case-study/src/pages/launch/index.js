a15a0e352913bbd71763468b352aa800542fe0b1
loreleim <lorelei.miyamura@gmail.com>
2020-02-28 17:16:56 -0600

    initial launch page

---
+++ wendy-ux-case-study/src/pages/launch/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./index.module.scss";
+
+class Launch extends React.Component {
+  render() {
+    return (
+      <div className={style.mainContainer}>
+        <div>Launch</div>
+      </div>
+    );
+  }
+}
+
+export default Launch;
