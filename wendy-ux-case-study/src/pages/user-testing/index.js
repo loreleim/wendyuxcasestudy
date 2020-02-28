d02213460f6acf34a2ff574949a07c4b6eea750f
loreleim <lorelei.miyamura@gmail.com>
2020-02-28 17:16:11 -0600

    initial user testing page

---
+++ wendy-ux-case-study/src/pages/user-testing/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./index.module.scss";
+
+class UserTesting extends React.Component {
+  render() {
+    return (
+      <div className={style.mainContainer}>
+        <div>User Testing</div>
+      </div>
+    );
+  }
+}
+
+export default UserTesting;
