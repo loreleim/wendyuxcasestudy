ac6526c64026bd40d813104bed707517e5b26493
loreleim <lorelei.miyamura@gmail.com>
2020-02-28 17:16:38 -0600

    initial planning page

---
+++ wendy-ux-case-study/src/pages/planning/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./index.module.scss";
+
+class Planning extends React.Component {
+  render() {
+    return (
+      <div className={style.mainContainer}>
+        <div>Planning Page</div>
+      </div>
+    );
+  }
+}
+
+export default Planning;
