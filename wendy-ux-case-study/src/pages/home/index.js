c5eb6b2901ace9dd615b19418474dbc849ae9cfe
loreleim <lorelei.miyamura@gmail.com>
2020-03-06 12:09:48 -0600

    fixes on the title svg

--- wendy-ux-case-study/src/pages/home/index.js
+++ wendy-ux-case-study/src/pages/home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import style from "./index.module.scss";
+import DownloadButton from "../../components/download-game";
 
 class Home extends React.Component {
   render() {
     return (
       <div className={style.mainContainer}>
         <section className={style.heroContainer}>
-          <h1>Home</h1>
+          <div className={style.mainTitleContainer}></div>
+          <DownloadButton></DownloadButton>
         </section>
       </div>
     );
